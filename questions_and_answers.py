4e0e77021e54779b3fdb93d280f64d936b20e3bd
pritam3339 <36327719+pritam3339@users.noreply.github.com>
2019-02-22 19:37:23 +0530

    Add files via upload

---
+++ questions_and_answers.py
@@ -0,0 +1,6 @@
+fields = { 
+    'question_1': ('some question, that is great --1','help-text for question-1'),
+    'question_2': ('some question, that is great --2','help-text for question-1'),
+    'question_3': ('some question, that is great --3','help-text for question-1'),
+    'question_4': ('some question, that is great --4','help-text for question-1'),
+}
