818ff69098427ed1595881b7ac3f4cf091652903
pritam3339 <pdeypritam@gmail.com>
2019-03-07 19:35:52 +0530

    remove unwanted files

--- slambook/settings.py
+++ slambook/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+from .local_settings import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -51,8 +51,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'pritams.slambook@gmail.com'
-EMAIL_HOST_PASSWORD ='PRITAMmatirp'
+EMAIL_HOST_USER = user
+EMAIL_HOST_PASSWORD = password
 
 
 
